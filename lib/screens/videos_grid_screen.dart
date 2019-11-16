978984b2f2773d53f6dcac3c6235c5b529008192
Andrew Bell <fiixed@gmail.com>
2019-11-16 16:41:12 -0600

    fixed gridview aspect ratio

--- lib/screens/videos_grid_screen.dart
+++ lib/screens/videos_grid_screen.dart
@@ -11,6 +11,12 @@ class VideosGridScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var mediaQueryData = MediaQuery.of(context);
+    final double widthScreen = mediaQueryData.size.width;
+    final double appBarHeight = kToolbarHeight;
+    final double paddingBottom = mediaQueryData.padding.bottom;
+    final double heightScreen =
+        mediaQueryData.size.height - paddingBottom - appBarHeight;
     return Scaffold(
       backgroundColor: Colors.black87,
       appBar: AppBar(
@@ -28,34 +34,31 @@ class VideosGridScreen extends StatelessWidget {
       body: FutureBuilder(
         future: Provider.of<VideosProvider>(context, listen: false)
             .fetchAndSetVideos(),
-        builder: (ctx, snapshot) => snapshot.connectionState ==
-                ConnectionState.waiting
-            ? Center(
-                child: CircularProgressIndicator(),
-              )
-            : Consumer<VideosProvider>(
-                child: Center(
-                  child: Text('Got no videos yet, start creating some!'),
-                ),
-                builder: (ctx, videosProvider, ch) =>
-                    videosProvider.items.length <= 0
-                        ? ch
-                        : GridView.builder(
-                            itemCount: videosProvider.items.length,
-                            gridDelegate:
-                                SliverGridDelegateWithFixedCrossAxisCount(
-                                    crossAxisCount: 2),
-                            itemBuilder: (BuildContext context, int index) {
-                              return GridTile(
-                                child: Container(
-                                  child: Image.file(
-                                      videosProvider.items[index].thumbnail),
-                                  color: Colors.black87,
-                                ),
-                              );
-                            },
-                          ),
-              ),
+        builder: (ctx, snapshot) =>
+            snapshot.connectionState == ConnectionState.waiting
+                ? Center(
+                    child: CircularProgressIndicator(),
+                  )
+                : Consumer<VideosProvider>(
+                    child: Center(
+                      child: Text('Got no videos yet, start creating some!'),
+                    ),
+                    builder: (ctx, videosProvider, ch) =>
+                        videosProvider.items.length <= 0
+                            ? ch
+                            : GridView.builder(
+                                itemCount: videosProvider.items.length,
+                                gridDelegate:
+                                    SliverGridDelegateWithFixedCrossAxisCount(
+                                        childAspectRatio:
+                                            widthScreen / heightScreen + 0.15,
+                                        crossAxisCount: 2),
+                                itemBuilder: (BuildContext context, int index) {
+                                  return Image.file(
+                                      videosProvider.items[index].thumbnail);
+                                },
+                              ),
+                  ),
       ),
     );
   }
